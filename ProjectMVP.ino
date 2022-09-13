2208d191b74e52b7b7f7788c5bbb15a7733fca1c
Andi Moser <andi@offline,ch>
2022-09-13 19:17:41 +0200

    Arbeit

--- ProjectMVP.ino
+++ ProjectMVP.ino
@@ -17,9 +17,15 @@
 //////////////////////////////////////////////////////////////////////
 // Configuration
 //////////////////////////////////////////////////////////////////////
-#define PIXELS_COUNT        20
-#define LOADCELL_OFFSET     50682624
-#define LOADCELL_DIVIDER    5895655
+#define PIXELS_COUNT                  10
+#define LOADSELL_UNITS_PER_GRAMM      187
+
+//////////////////////////////////////////////////////////////////////
+// Calibration
+//////////////////////////////////////////////////////////////////////
+#define WEIGHT_THRESHOLD_EMPTY 100
+#define WEIGHT_THRESHOLD_FULL 100
+
 
 //////////////////////////////////////////////////////////////////////
 // Global Variables
@@ -34,6 +40,9 @@ long currentRead;
 
 long emptyWeight = 0;
 long fullWeight = 0;
+long contentWeight = 0;
+long drankWeight = 0;
+String currentState = "";
 
 //////////////////////////////////////////////////////////////////////
 // States
@@ -42,11 +51,16 @@ State* s_Init = machine.addState(&stateInit);
 State* s_Calibrate_Empty = machine.addState(&stateCalibrateEmpty);
 State* s_Empty_Done = machine.addState(&stateEmptyDone);
 State* s_Calibrate_Full = machine.addState(&stateCalibrateFull);
-State* s_Drinking = machine.addState(&stateDrinking);
+State* s_DrinkingIdle = machine.addState(&stateDrinkingIdle);
+State* s_DrinkingLiftup = machine.addState(&stateDrinkingLiftup);
 
 // ****** StateInit
 int currentLedInit = 0;
 void stateInit() {
+  if (machine.executeOnce) {
+    M5.Lcd.fillScreen(BLACK);
+  }
+  currentState = "Init";
   showOnlySingleLED(currentLedInit++, g_Pixels.Color(0, 0, 255));
   if (currentLedInit == PIXELS_COUNT) {
     currentLedInit = 0;
@@ -60,6 +74,7 @@ int calibrateEmptyFrom = 0;
 int calibrateEmptyUntil = 0;
 int calibrateEmptyCounter = 0;
 void stateCalibrateEmpty() {
+  currentState = "CalibrateEmpty";
   if (machine.executeOnce) {
     calibrationEmptyDone = false;
     calibrateEmptyCounter = 0;
@@ -74,7 +89,7 @@ void stateCalibrateEmpty() {
   }
   
   totalEmptyReads += getReading();
-  if (calibrateEmptyCounter++ == PIXELS_COUNT) {
+  if (++calibrateEmptyCounter == PIXELS_COUNT) {
     emptyWeight = (long)(totalEmptyReads/PIXELS_COUNT);
     calibrationEmptyDone = true;
   }
@@ -82,6 +97,7 @@ void stateCalibrateEmpty() {
 
 int currentLedEmptyDone = 0;
 void stateEmptyDone() {
+  currentState = "EmptyDone";
     showOnlySingleLED(currentLedEmptyDone++, g_Pixels.Color(0, 255, 0));
   if (currentLedEmptyDone == PIXELS_COUNT) {
     currentLedEmptyDone = 0;
@@ -95,6 +111,7 @@ int calibrateFullFrom = 0;
 int calibrateFullUntil = 0;
 int calibrateFullCounter = 0;
 void stateCalibrateFull() {
+  currentState = "CalibrateFull";
   if (machine.executeOnce) {
     calibrationFullDone = false;
     calibrateFullFrom = currentLedEmptyDone;
@@ -109,32 +126,45 @@ void stateCalibrateFull() {
   }
 
   totalFullReads += getReading();
-  if (calibrateFullCounter++ == PIXELS_COUNT) {
+  if (++calibrateFullCounter == PIXELS_COUNT) {
     fullWeight = (long)(totalFullReads/PIXELS_COUNT);
+    contentWeight = fullWeight - contentWeight;
     calibrationFullDone = true;
   }
 }
 
-// ****** StateDrinking
+// ****** StateDrinkingIdle
 int currentPixelsShowing = 0;
-void stateDrinking() {
-  if (machine.executeOnce) {
-    lightAllLeds(g_Pixels.Color(0, 255, 0));
-  }
-
+float ratio = 0;
+void stateDrinkingIdle() {
+  currentState = "DrinkingIdle";
   long valueRaw = currentRead - emptyWeight;
 
-  float ratio = (float)valueRaw / (float)fullWeight;
-  int ledsToShow = round((float)ratio * (float)PIXELS_COUNT);
+  //M5.Lcd.println("currentRead: " + String(currentRead));
+//  ratio = round2((float)currentRead / (float)fullWeight);
 
-  Serial.println(String(ledsToShow) + "\t\t" + String(ratio) + "\t\t" + String(currentRead));
+
+  drankWeight = fullWeight - currentRead;
+  ratio = round2((float)(drankWeight/contentWeight));
+
+  float ratioRed = ratio;
+  float ratioGreen = 1 - ratioRed;
+
+  lightAllLeds(g_Pixels.Color(round(ratioRed * 255), round(ratioGreen * 255), 0));
+  /*int ledsToShow = round((float)ratio * (float)PIXELS_COUNT);
 
   if (ledsToShow != currentPixelsShowing) {
     currentPixelsShowing = ledsToShow;
 
     showLEDUntil(currentPixelsShowing, g_Pixels.Color(0, 255, 0));
-  }
+  }*/
 
+}
+
+// ******* stateDrinkingLiftup
+void stateDrinkingLiftup() {
+  currentState = "DrinkingLiftup";
+  lightAllLeds(g_Pixels.Color(0,0,0));
 }
 
 
@@ -144,7 +174,7 @@ void stateDrinking() {
 
 int t_initToCalibrateEmpty_Counter = 0;
 bool t_initToCalibrateEmpty() {
-  if (currentRead > 2000) {
+  if (currentRead > WEIGHT_THRESHOLD_EMPTY) {
     t_initToCalibrateEmpty_Counter++;
 
     if (t_initToCalibrateEmpty_Counter > 10) {
@@ -162,7 +192,7 @@ bool t_EmptyCalibrationToEmptyDone() {
 
 int t_EmptyDoneToFull_Counter = 0;
 bool t_EmptyDoneToFull() {
-    if (currentRead > (2000 + emptyWeight)) {
+    if (currentRead > (WEIGHT_THRESHOLD_FULL + emptyWeight)) {
     t_EmptyDoneToFull_Counter++;
 
     if (t_EmptyDoneToFull_Counter > 10) {
@@ -189,8 +219,25 @@ bool t_FullToDrinking() {
   return false;
 }
 
-bool t_DrinkingToInit() {
-  return currentRead < emptyWeight || abs(currentRead - emptyWeight) < 2000;
+bool t_DrinkingIdleToLiftUp() {
+  return currentRead < 10;
+}
+
+bool t_LiftUpToDrinkingIdle() {
+  return currentRead > emptyWeight;
+}
+
+int t_DrinkingIdleToInit_Counter = 0;
+bool t_DrinkingIdleToInit() {
+  if (abs(currentRead - emptyWeight) < 5) { // if, for 10 ticks, the difference between the currentRead and emptyWeight is less than 50g
+    t_DrinkingIdleToInit_Counter++;
+
+    if (t_DrinkingIdleToInit_Counter == 10) {
+      t_DrinkingIdleToInit_Counter = 0;
+      return true;
+    }
+  }
+  return false;
 }
 
 int t_ToReset_Counter = 0;
@@ -199,7 +246,7 @@ bool t_ToReset() {
     t_ToReset_Counter = 0;
   }
 
-  if (currentRead > (fullWeight + 30000)) {
+  if (currentRead > (fullWeight + 1000)) { // 1kg above the full weight
     t_ToReset_Counter++;
 
     if (t_ToReset_Counter > 10) {
@@ -217,21 +264,43 @@ void setup() {
 
   g_LoadCell.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
 
-  tareFactor = g_LoadCell.read();
+  long tareTotal = 0;
+  for (int i = 0; i < 20; i++) {
+    tareTotal += g_LoadCell.read();
+  }
+  tareFactor = (long)(tareTotal/20);
   g_Pixels.begin();
 
   s_Init->addTransition(&t_initToCalibrateEmpty, s_Calibrate_Empty);
   s_Calibrate_Empty->addTransition(&t_EmptyCalibrationToEmptyDone, s_Empty_Done);
   s_Empty_Done->addTransition(&t_EmptyDoneToFull, s_Calibrate_Full);
-  s_Calibrate_Full->addTransition(&t_FullToDrinking, s_Drinking);
-  s_Drinking->addTransition(&t_ToReset, s_Init);
-  s_Drinking->addTransition(&t_DrinkingToInit, s_Init);
+  s_Calibrate_Full->addTransition(&t_FullToDrinking, s_DrinkingIdle);
+  s_DrinkingIdle->addTransition(&t_DrinkingIdleToInit, s_Init);
+  s_DrinkingIdle->addTransition(&t_DrinkingIdleToLiftUp, s_DrinkingLiftup);
+  s_DrinkingLiftup->addTransition(&t_LiftUpToDrinkingIdle, s_DrinkingIdle);
+
+  // Resets
+  s_DrinkingIdle->addTransition(&t_ToReset, s_Init);
+  s_DrinkingLiftup->addTransition(&t_ToReset, s_Init);
 
 }
 
 void loop() {
-  lastRead = currentRead;
-  currentRead = getReading();
+  long thisRead = getReading();
+
+  if (abs(thisRead - lastRead) > 10) { // only detect changes above 10g
+    lastRead = currentRead;
+    currentRead = thisRead;
+  }
+
+//  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.setCursor(0, 0);
+  M5.Lcd.println("State: " + currentState + "      ");
+  M5.Lcd.println("fullWeight: " + String(fullWeight) + "     ");
+  M5.Lcd.println("emptyWeight: " + String(emptyWeight) + "      ");
+  M5.Lcd.println("drinkStatus: " + String(drankWeight) + " / " + String(contentWeight) + "        ");
+  M5.Lcd.println("currentRead: " + String(currentRead) + "       ");
+  M5.Lcd.println("ratio: " + String(ratio));
   
   machine.run();
   delay(200);
@@ -277,5 +346,11 @@ void showLEDFromUntil(int from, int until, uint32_t color) {
 }
 
 long getReading() {
-  return g_LoadCell.read() - tareFactor;
+  return (long)((g_LoadCell.read() - tareFactor)/2/LOADSELL_UNITS_PER_GRAMM);
+}
+
+float round2(float var)
+{
+    float value = (int)(var * 100 + .5);
+    return (float)value / 100;
 }
---
+++ readme.md
@@ -0,0 +1,45 @@
+# SmartCoaster
+
+## Das Projekt
+
+SmartCoaster wurde als Projekt im Rahmen des Faches "Embedded Systems" an der Teko Luzern durchgeführt.
+
+Aufgabenstellung des Projektes war es, mit einem ESP32 die Daten von mindestens einem Sensor auszuwerten und
+über ein Output Device darzustellen.
+
+### Zielsetzung
+
+(...) Folgt
+
+## Die Hardware
+
+### Beetle ESP32
+
+### HX711 Load Cell Amplifier
+
+### 5kg Load Cell Gewichtssensor
+
+### WS2812B NeoPixel LED Strip
+
+## Aufbau der Hardware
+
+### LEDs
+
+- Roter Pin: 3.3V
+- Schwarzer Pin: GND
+- Grüner Pin: via 330 Ohm Widerstand zum ESP Pin 2
+
+### HX711
+
+#### Seite ESP32
+
+- GND - GND
+- DT - ESP Pin 16
+- SCK - ESP Pin 17
+
+#### Seite Load Cell
+
+- E+ - Rotes Kabel
+- E- - Schwarzes Kabel
+- A+ - Weisses Kabel
+- A- - Grünes oder gelbes Kabel
